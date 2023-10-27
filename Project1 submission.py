da2db5c20ea7f37d067e8c764de3e2adf5957cbd
GGDoctor <127359046+GGDoctor@users.noreply.github.com>
2023-10-26 17:11:48 -0700

    Fixed 1A and fixed main

--- Project1 submission.py
+++ Project1 submission.py
@@ -24,7 +24,7 @@ def Problem1a(n):
 """
 Problem 1b
 Write a function Fibonacci that takes as input a positive integer n and returns
-the Fn. We define Fn in the usual way, namely Fm = Fm−1 + Fm−2 with one change: The first
+the Fn. We define Fn in the usual way, namely Fm = Fm-1 + Fm-2 with one change: The first
 two values of the sequence are F0 =1/2 and F2 =1/3.
 """
 def Fibonacci(n):
@@ -41,7 +41,7 @@ The goal of this problem is to implement a modified primality test primality3: G
 input an integer N and confidence parameter k, first test if N is divisible by 3, 5, 7 or 11. If it
 is divisible by any of these numbers output(”no”); else call primality2 with N and k as inputs.
 (This in turn calls primality algorithm that randomly chooses a (where 1 < a < N) and tests
-if a^N−1 ≡ 1(mod N) and repeats it k times to reduce the probability of error to 2^-k.)
+if a^N-1 ≡ 1(mod N) and repeats it k times to reduce the probability of error to 2^-k.)
 """
 def primality2(n,k):
     if (n == 1):
@@ -57,9 +57,7 @@ def primality2(n,k):
 
 
 def primality3(n,k):
-    #n = int(input("Enter a integer: "))
 
-    #k = int(input("Enter a confidence perameter: "))
     if (n%3==0 or n%5==0 or n%7==0 or n%11==0):
        print("No")
     else:
@@ -69,12 +67,12 @@ def primality3(n,k):
 """
 Problem 3
 Given integer k, generate a random prime number with n bits. Implement a solution as
-follows: generate a random k − 2 binary string and add 1 as the first and the last bit to create
+follows: generate a random k - 2 binary string and add 1 as the first and the last bit to create
 a k bit integer and convert it to decimal. (The reason for the first bit to be 1 is that we want
-the number to be odd. The last bit should be 1 since we want no leading 0’s.) Then, call
+the number to be odd. The last bit should be 1 since we want no leading 0's.) Then, call
 primality3 algorithm you in implemented in Problem 1 with n and k as inputs. (k is the second
-parameter in primality2 which is the number of times the Fermat’s test is repeated.) Repeat
-calling primaility3 until it outputs ’yes’. At this point, you have found a prime number (with
+parameter in primality2 which is the number of times the Fermat's test is repeated.) Repeat
+calling primaility3 until it outputs 'yes'. At this point, you have found a prime number (with
 a high probability if k is large enough).
 """
 
@@ -142,8 +140,8 @@ Problem 4
 Given integers n and k, call the algorithm for Problem 3 (with n and k as inputs) to generate
 two primes p and q with n bits each, and use them to generate the encryption keys E and the
 decryption key D. After computing p and q, compute N = pq. Then, find a random 10 bit
-integer E such that gcd (E, (p − 1)(q − 1)) = 1. Next, find D such that DE ≡ 1 (mod N)
-using extended Euclid’s algorithm. Output N, E and D.
+integer E such that gcd (E, (p - 1)(q - 1)) = 1. Next, find D such that DE ≡ 1 (mod N)
+using extended Euclid's algorithm. Output N, E and D.
 """
 
 def gcd(a, b):
@@ -209,48 +207,44 @@ def RSA(N, E, D, M):
 def main():
     # Get the input from the user asking them what problem they would like solved
     print("----Questions----\n  1A\t 1B\t 2\t 3\t 4\t 5\t")
-    quest = str(input("Enter the problem you would like to answer: "))
-
+    quest = ""
     # Switch Statement
-    match quest:
-        case "1A":
-            n = int(input("Enter a positive integer: "))
-            print(Problem1a(n))
+    while (quest != "6"):
+        quest = str(input("Enter the problem you would like to answer: "))
+
+        match quest:
+            case "1A":
+                n = int(input("Enter a positive integer: "))
+                print(Problem1a(n))
+                
+            case "1B":
+                n = int(input("Enter a positive integer: "))
+                print(Fibonacci(n))
+
+            case "2":
+                n = int(input("Enter a integer: "))
+                k = int(input("Enter a confidence perameter: "))
+                print(primality3(n,k))
+
+            case "3":
+                n = int(input("Enter a positive integer: "))
+                k = int(input("Enter an integer k: " ))
+                print(primeGen(n, k))
+
             
-        case "1B":
-            n = int(input("Enter a positive integer: "))
-            print(Fibonacci(n))
-
-        case "2":
-            n = int(input("Enter a integer: "))
-            k = int(input("Enter a confidence perameter: "))
-            print(primality3(n,k))
-
-        case "3":
-            n = int(input("Enter a positive integer: "))
-            k = int(input("Enter an integer k: " ))
-            print(primeGen(n, k))
-
+            case "4":
+                n = int(input("Enter a positive integer: "))
+                k = int(input("Enter an integer k: " ))
+                print(genKeys(n,k))
         
-        case "4":
-             n = int(input("Enter a positive integer: "))
-             k = int(input("Enter an integer k: " ))
-             print(genKeys(n,k))
-    
-        case "5":
-            N = int(input("Enter the "))
-            E = int(input("Enter the encryption key: "))
-            D = int(input("Enter the decryption key: "))
-            M = int(input("Enter the message you would like to encode: "))
-            RSA(N, E, D, M)
-    
-    # Call the function the user selected
-
-    #question = input("Enter the problem you would like to answer: ")
-    #primality3()
-
-    #n = int(input("Enter a positive integer: ")) #Fibonacci input
-    #print(Fibonacci(n))
+            case "5":
+                N = int(input("Enter the "))
+                E = int(input("Enter the encryption key: "))
+                D = int(input("Enter the decryption key: "))
+                M = int(input("Enter the message you would like to encode: "))
+                RSA(N, E, D, M)
+            case "6":
+                print("Goodbye")
 
 if __name__=='__main__':
     main()
